1b12abe6f3aedad0a5f5db6aabcf119a52778bc9
amangeldiyev <amangeldiyev.amantur@gmail.com>
2025-06-03 10:03:58 +0500

    wip

--- resources/views/audits/safety-behaviour.blade.php
+++ resources/views/audits/safety-behaviour.blade.php
@@ -14,17 +14,17 @@
                             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
                                     <img src="/storage/{{setting('site.key_safety_image_1')}}" alt="Behaviour Image" class="object-cover rounded mb-4" style="width: 100%; height: auto; max-height: 150px;">
-                                    <h1 class="text-lg font-semibold text-gray-900 dark:text-gray-200">Communication</h1>
+                                    <h1 class="text-lg font-semibold text-gray-900 dark:text-gray-200">BE MINDFUL</h1>
                                     <span class="text-sm text-gray-700 dark:text-gray-300">{{setting('site.key_safety_text_1')}}</span>
                                 </div>
                                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
                                     <img src="/storage/{{setting('site.key_safety_image_2')}}" alt="Behaviour Image" class="object-cover rounded mb-4" style="width: 100%; height: auto; max-height: 150px;">
-                                    <h1 class="text-lg font-semibold text-gray-900 dark:text-gray-200">Risk assessment</h1>
+                                    <h1 class="text-lg font-semibold text-gray-900 dark:text-gray-200">GET INVOLVED</h1>
                                     <span class="text-sm text-gray-700 dark:text-gray-300">{{setting('site.key_safety_text_2')}}</span>
                                 </div>
                                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
                                     <img src="/storage/{{setting('site.key_safety_image_3')}}" alt="Behaviour Image" class="object-cover rounded mb-4" style="width: 100%; height: auto; max-height: 150px;">
-                                    <h1 class="text-lg font-semibold text-gray-900 dark:text-gray-200">Involvement</h1>
+                                    <h1 class="text-lg font-semibold text-gray-900 dark:text-gray-200">SPEAK OUT</h1>
                                     <span class="text-sm text-gray-700 dark:text-gray-300">{{setting('site.key_safety_text_3')}}</span>
                                 </div>
                             </div>
