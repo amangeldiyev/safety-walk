693cc5807864c2c7f4c5e6589b3cda261714c0d1
amangeldiyev <amangeldiyev.amantur@gmail.com>
2025-08-05 01:50:40 +0500

    Audit export

--- app/Exports/AuditsExport.php
+++ app/Exports/AuditsExport.php
@@ -2,30 +2,54 @@
 
 namespace App\Exports;
 
+use App\Enums\AuditMode;
 use App\Models\Audit;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class AuditsExport implements FromCollection, WithHeadings
+class AuditsExport implements FromCollection, WithHeadings, WithMapping, ShouldAutoSize
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Audit::all();
+        return Audit::with('site', 'user', 'contactUser')->get();
     }
-
 
     public function headings(): array
     {
         return [
             'ID',
-            'Name',
-            'Description',
-            'Created At',
-            'Updated At',
+            'Submitted On',
+            'Submitted By',
+            'User Email',
+            'Site',
+            'Safety Walk Mode',
+            'Good Practice',
+            'Points of Improvement',
+            'Overall Comments',
         ];
     }
-    
+
+    /**
+    * @param Audit $audit
+    */
+    public function map($audit): array
+    {
+        return [
+            $audit->id,
+            $audit->date,
+            $audit->user->name,
+            $audit->user->email,
+            $audit->site->name,
+            $audit->mode === AuditMode::CONVERSATION ? 'Conversation' : 'Guided',
+            $audit->good_practice ? '1' : '0',
+            $audit->point_of_improvement ? '1' : '0',
+            $audit->comment
+        ];
+    }
+
 }
